581649276fd2f00cc46e3b7bf1c98849ef291215
Yiğit Arpacıoğlu <yigit.arpacioglu@gmail.com>
2021-01-24 21:13:58 +0300

    Add project files.

---
+++ DamaTahtasiDemo/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DamaTahtasiDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(420, 440);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8];
+            int top = 0;
+            int left = 0;
+
+            for (int i = 0; i < buttons.GetUpperBound(0)+1; i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1)+1; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    this.Controls.Add(buttons[i, j]);
+                    if ((i+j)%2==0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    left += 50;
+                }
+
+                left = 0;
+                top += 50;
+            }
+        }
+    }
+}
